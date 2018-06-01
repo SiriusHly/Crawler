3560a1ef4710e00e79bd6cfcfe4b8fdeda5451f8
黄良运 <1136513099@qq.com>
2018-06-02 02:59:48 +0800

    完成抓取区块链

--- WebMagic/src/main/java/com/hly/webmagic/Spider/MyWebMagic.java
+++ WebMagic/src/main/java/com/hly/webmagic/Spider/MyWebMagic.java
@@ -19,17 +19,15 @@ public class MyWebMagic implements PageProcessor {
     public void process(Page page) {
 
         List<String> listTitle = page.getHtml().xpath("//div[@class='title']/h2/a/text()").regex(".*区块链.*").all();
-
-        List<String> listBlock = page.getHtml().xpath("//div[@class='title']/h2").regex("<a.*href=\"(.*)\".*</a>").links().all();
+        //不用括号括起来，就全部返回
+        List<String> listBlock = page.getHtml().xpath("//div[@class='title']/h2").regex("<a\\b[^>]+\\bhref=\"([^\"]*)\"[^>]*>.*区块链.*</a>").all();
 
         for(int i =0;i<listTitle.size();i++)
-            System.out.println(listTitle.get(i)+" :"+listBlock);
+            System.out.println(listTitle.get(i)+" :"+listBlock.get(i));
     }
-
     public Site getSite() {
         return site;
     }
-
     public static void main(String[] args) {
         Spider.create(new MyWebMagic()).addUrl("https://blog.csdn.net/nav/blockchain")
                 .addPipeline(new ConsolePipeline()).run();
